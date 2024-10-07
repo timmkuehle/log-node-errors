95efc8b7afd93b754c29e7a23a204c5745d3390d
Tim Moritz Kühle <tim-moritz.kuehle@sirum.de>
2024-10-07 23:53:52 +0200

    Added test code in index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import { NodeErrorBoundary } from "./NodeErrorBoundary";
+
 export { hans } from "./hans";
 
 export { wurst } from "./wurst";
+
+const errorBoundary = new NodeErrorBoundary();
+
+errorBoundary.execute(() => {
+	throw Error("Das doof jetzt...");
+});
